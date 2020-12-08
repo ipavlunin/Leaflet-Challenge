e7f1bb10c6b5256e41ffb4fcd6cafbaea6c65320
ipavlunin <ipavlunin@gmail.com>
2020-12-08 12:12:30 -0600

    outdoor layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -32,5 +32,10 @@ function createMap(earthquakes, tectonics) {
         id: "dark-v10",
         accessToken: API_KEY
     });
-
+    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "outdoors-v9",
+        accessToken: API_KEY
+    });
 }
