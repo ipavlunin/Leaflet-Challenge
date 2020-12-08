aa3e594577e269ee3b7b934e0303f22d763acf15
ipavlunin <ipavlunin@gmail.com>
2020-12-08 12:11:58 -0600

    darkmap

--- static/js/logic.js
+++ static/js/logic.js
@@ -26,6 +26,11 @@ function createMap(earthquakes, tectonics) {
         id: "mapbox/streets-v11",
         accessToken: API_KEY
     });
-
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
+    });
 
 }
