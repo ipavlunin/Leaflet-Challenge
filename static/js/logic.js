82e9a006c964e856d4c52e645f48403fdfc7641b
ipavlunin <ipavlunin@gmail.com>
2020-12-08 12:35:39 -0600

    satmap added

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,8 @@
 
 // API URLs to get earthquakes data and tectonic plates boundaries.
 var link1 = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-var link2 = "https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_plates.json";
+var link2 = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+// var link2 = 'static/data/PB2002_plates.json'
 
 // GET request to get data from the URLs
 const earthquake = d3.json(link1, function (earthquakeData) {
@@ -11,10 +12,50 @@ const earthquake = d3.json(link1, function (earthquakeData) {
     })
 })
 
+function createFeatures(earthquakeDataFeatures, tectonicDataFeatures) {
+    function eqLabels(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + Date(feature.properties.time) + "</p><hr><p>" + "Magnitude: " + feature.properties.mag + "</p>");
+    }
+    function tecLables(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.Name + "</h3><h4> PlateA: " + feature.properties.PlateA + "<br>PlateB:" + feature.properties.PlateB + "</h4> <hr><p>" + feature.properties.Source + "</p>");
+    }
+    var color = "";
+    var earthquakes = L.geoJSON(earthquakeDataFeatures, {
+        pointToLayer: function (feature, latlng) {
+
+            if (feature.properties.mag > 5) {
+                color = "#cc0000";
+            } else if (feature.properties.mag > 4) {
+                color = "#cc6600";
+            } else if (feature.properties.mag > 3) {
+                color = "#ff8000";
+            } else if (feature.properties.mag > 2) {
+                color = "#ffff33";
+            } else if (feature.properties.mag > 1) {
+                color = "#ffff66";
+            } else if (feature.properties.mag < 1) {
+                color = "#b2ff66";
+            }
+            radius = feature.properties.mag * 20000;
+
+            return L.circle(latlng, { radius: radius, color: color, fillOpacity: 0.75, fillcolor: color })
+        }, onEachFeature: eqLabels
+    });
+    var tectonics = L.geoJSON(tectonicDataFeatures, {
+        style: function (feature) {
+            return {
+                color: "orange",
+                weight: 2
+            };
+        },
+        onEachFeature: tecLables
+    });
+    createMap(earthquakes, tectonics);
+}
+
 // Function to create a map and define map layers
 function createMap(earthquakes, tectonics) {
-
-    var streetmap = L.layer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
         maxZoom: 18,
@@ -26,6 +67,12 @@ function createMap(earthquakes, tectonics) {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
         id: "dark-v10",
+        accessToken: API_KEY
+    });
+    var satmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "satellite-v9",
         accessToken: API_KEY
     });
     var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
