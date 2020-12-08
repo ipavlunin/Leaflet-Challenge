791668dbf0e14f3904e85d424e8135bb9cbceea2
ipavlunin <ipavlunin@gmail.com>
2020-12-08 12:17:13 -0600

    layer control

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,13 +1,10 @@
 /// Application to visualize earthquake (last 7 days) as a layers on a earth map ///
 
 // API URLs to get earthquakes data and tectonic plates boundaries.
-
 var link1 = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
 var link2 = "https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_plates.json";
 
 // GET request to get data from the URLs
-
 const earthquake = d3.json(link1, function (earthquakeData) {
     d3.json(link2, function (tectonicData) {
         createFeatures(earthquakeData.features, tectonicData.features);
@@ -15,7 +12,6 @@ const earthquake = d3.json(link1, function (earthquakeData) {
 })
 
 // Function to create a map and define map layers
-
 function createMap(earthquakes, tectonics) {
 
     var streetmap = L.layer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -59,4 +55,9 @@ function createMap(earthquakes, tectonics) {
         zoom: 3,
         layers: [satmap, earthquakes, tectonics]
     });
+
+    // Layer control
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
 }
